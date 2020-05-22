dd3de1944086f5162a9e9d5a117669440eb60626
Ilya <mu.ilya.danilov.1999@mail.ru>
2020-05-22 19:17:10 +0300

    first commit

---
+++ lab9/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab9
+{
+    public partial class Form1 : Form
+    {
+        Graphics gr;
+        Pen p;
+        SolidBrush fon;
+        SolidBrush fig;
+        int rad = 20;
+        Random rand;
+        List<IPoint> Arr = new List<IPoint>();
+        int num = 1;
+        int kof = 5;
+        public Form1()
+        {
+            InitializeComponent();
+            rand = new Random();
+        }
+        void DrawCircle(int x, int y)
+        {
+            int xc, yc;
+            xc = x - rad;
+            yc = y - rad;
+            gr.FillEllipse(fig, xc, yc, rad, rad);
+            gr.DrawEllipse(p, xc, yc, rad, rad);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gr = pictureBox1.CreateGraphics();
+            p = new Pen(Color.Black);
+            fon = new SolidBrush(Color.LightGray);
+            fig = new SolidBrush(Color.Black);
+            Arr = new List<IPoint>();
+            num = 1;
+            gr.FillRectangle(fon, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            int x, y, vx, vy;
+            for (int i = 0; i < Convert.ToInt32(textBox1.Text); i++)
+            {
+                x = rand.Next(rad, pictureBox1.Width - rad);
+                y = rand.Next(rad, pictureBox1.Height - rad);
+                vx = rand.Next(1, 5);
+                vy = rand.Next(1, 5);
+                if (rand.Next(1, 2) == 1)
+                {
+                    vx = -vx;
+                }
+                if (rand.Next(1, 2) == 1)
+                {
+                    vy = -vy;
+                }
+                if (num == 1)
+                {
+                    Arr.Add(new IPoint(rad, x, y, vx, vy, pictureBox1.Width, pictureBox1.Height, num));
+                    num++;
+                    DrawCircle(x, y);
+                }
+                else
+                {
+                    if (Arr.Last().Chek(rad, x, y, Arr))
+                    {
+                        Arr.Add(new IPoint(rad, x, y, vx, vy, pictureBox1.Width, pictureBox1.Height, num));
+                        num++;
+                        DrawCircle(x, y);
+                    }
+                    else
+                    {
+                        i--;
+                    }
+                }
+            }
+            timer1.Interval = 50;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            List<Task> task = new List<Task>();
+            IPoint[] ArrK = new IPoint[Arr.Count];
+            Arr.CopyTo(ArrK);
+            foreach (var i in Arr)
+            {
+                task.Add(new Task(() => i.Update(ArrK)));
+                task.Last().Start();
+            }
+            Task.WaitAll(task.ToArray());
+            gr.FillRectangle(fon, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            foreach (var i in Arr)
+            {
+                i._svx = i._vx;
+                i._svy = i._vy;
+                i._sx = i._x;
+                i._sy = i._y;
+                DrawCircle(i._x, i._y);
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            foreach (var i in Arr)
+            {
+                i.ChSpd(trackBar1.Value - kof);
+                kof = trackBar1.Value;
+            }
+            timer1.Enabled = true;
+        }
+    }
+}
---
+++ lab9/IPoint.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    public class IPoint
+    {
+        public int _rad;
+        public int _x;
+        public int _y;
+        public int _sx;
+        public int _sy;
+        public int _vx;
+        public int _vy;
+        public double k;
+        int _maxx;
+        int _maxy;
+        public int _num;
+        public int _svx;
+        public int _svy;
+        public IPoint(int rad, int x, int y, int vx, int vy, int maxx, int maxy, int num)
+        {
+            _rad = rad;
+            _x = x;
+            _y = y;
+            _vx = vx;
+            _vy = vy;
+            _svx = vx;
+            _svy = vy;
+            _maxx = maxx;
+            _maxy = maxy;
+            _num = num;
+        }
+        public void Update(IPoint[] Arr)
+        {
+            foreach (var i in Arr)
+            {
+                if (i._num != _num)
+                {
+                    if (Math.Abs((_x) - (i._sx)) <= _rad && Math.Abs((_y) - (i._sy)) <= _rad)
+                    {
+                        _vx = i._svx;
+                        _vy = i._svy;
+                    }
+                }
+            }
+            if (_x + _vx + _rad >= _maxx || _x + _vx - _rad <= 0)
+            {
+                _vx = -_vx;
+            }
+            if (_y + _vy + _rad >= _maxy || _y + _vy - _rad <= 0)
+            {
+                _vy = -_vy;
+            }
+            _x += _vx;
+            _y += _vy;
+        }
+        public bool Chek(int rad, int x, int y, List<IPoint> Arr)
+        {
+            foreach (var i in Arr)
+            {
+                if (Math.Abs((x) - (i._x)) <= _rad && Math.Abs((y) - (i._y)) <= _rad)
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+        public void ChSpd(int k)
+        {
+            if (_vx >= 0)
+            {
+                if (_vx + k > 0)
+                {
+                    _vx = _vx + k;
+                    _svx = _vx + k;
+                }
+                else
+                {
+                    _vx = 0;
+                    _svx = 0;
+                }
+            }
+            else
+            {
+                if (_vx + k < 0)
+                {
+                    _vx = _vx - k;
+                    _svx = _vx - k;
+                }
+                else
+                {
+                    _vx = 0;
+                    _svx = 0;
+                }
+            }
+            if (_vy >= 0)
+            {
+                if (_vy + k > 0)
+                {
+                    _vy = _vy + k;
+                    _svy = _vy + k;
+                }
+                else
+                {
+                    _vy = 0;
+                    _svy = 0;
+                }
+            }
+            else
+            {
+                if (_vy + k < 0)
+                {
+                    _vy = _vy - k;
+                    _svy = _vy - k;
+                }
+                else
+                {
+                    _vy = 0;
+                    _svy = 0;
+                }
+            }
+        }
+    }
+}
